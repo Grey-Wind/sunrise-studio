43bc948d76bb0d7485f0c56f649ae1f2b3eb5c7d
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-16 19:32:36 +0800

    use vue

---
+++ dev.bat
@@ -0,0 +1,3 @@
+@echo off
+start http://localhost:8080/
+npx vuepress dev sunrise-studio
---
+++ sunrise-studio/.vuepress/.temp/internal/pagesRoutes.js
@@ -0,0 +1,4 @@
+export const pagesRoutes = [
+  ["v-8daa1a0e","/",{"title":"欢迎来到我的网站"},["/index.md"]],
+  ["v-3706649a","/404.html",{"title":""},[]],
+]
