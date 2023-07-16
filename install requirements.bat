235c4de42e02bcc2ec03e40fba70a6366bd6bd05
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-16 15:43:59 +0800

    update butterfly

--- install requirements.bat
+++ install requirements.bat
@@ -1,2 +1,4 @@
 npm install -g hexo-cli
 npm install hexo-theme-butterfly --save
+git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
+npm install hexo-renderer-pug hexo-renderer-stylus --save
