eec8209238399c17639ba3be895015b92fd9c068
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-16 19:47:41 +0800

    delete vue

--- dev.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start http://localhost:8080/
-npx vuepress dev sunrise-studio
--- sunrise-studio/.vuepress/.temp/internal/pagesRoutes.js
+++
@@ -1,4 +0,0 @@
-export const pagesRoutes = [
-  ["v-8daa1a0e","/",{"title":"欢迎来到我的网站"},["/index.md"]],
-  ["v-3706649a","/404.html",{"title":""},[]],
-]
